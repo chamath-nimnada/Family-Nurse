5746d9c521a57b249170b3d24bb3d7c00274ab86
Chamath Nimnada <nimnadachamath25@gmail.com>
2026-01-13 18:15:57 +0530

    updated

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Family-Nurse/', // Replace with your repository name
+})
